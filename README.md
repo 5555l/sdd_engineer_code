7831981a9f632c6cd0b039e4c7654d8a7b21fd63
Jules <five@theinfamous.me>
2022-11-20 16:38:54 +0000

    updated

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Generate SDD engineer passwords for functions such as CCF editing
 
 Tested on VINs ending:
+
 - Rxxxxx (Jag)
 
  Feedback about working with other VIN's would be useful
- 
--- sdd_key.py
+++ sdd_key.py
@@ -64,7 +64,7 @@ def seedgen(vin,dt):
 cipher_key = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 s_cipher =   'G7HM8CPLFAQW2R9Y1DE3SVU4O5KTJB6XNIZ0'
 p_cipher =   'CPLFAQW2R9Y1DB3SVU4E5K8JO6XNGZTH7IM0'
-# These two ciphers are *almost* the same. It could be there are the same and
+# These two ciphers are *almost* the same. It could be they are the same and
 # the mismatched values are VIN's that are never used IRL so it doesn't matter
 
 # Set seed and password cipher dictionaries 
